hm-edu/alertmanager-hookshot-adapter

commit c2e571cdaee4679c684fb388166da5a2f163cba0

fix: better handling of large amount of alerts

index.ts
@@ -75,13 +75,13 @@ interface AlertData {
     ]
 
 }
-function transform(data: AlertData): { version: string, empty: boolean } | { version: string, plain: string, html: string, msgtype: string } {
+function transform(data: AlertData): { version: string, empty: boolean, msgtype: string }[] | { version: string, plain: string, html: string, msgtype: string }[] {
     if (!data.alerts) {
-        return {
+        return [{
             version: 'v2',
             empty: true,
             msgtype: 'm.text'
-        };
+        }];
     }
 
     const plainErrors = [];
@@ -112,12 +112,16 @@ function transform(data: AlertData): { version: string, empty: boolean } | { ver
              </p>
              ${(alert.status === "firing") ? `<p>${silenceLink(alert, grafanaUrl)}</p>` : ''}`)
     }
-    return {
-        version: 'v2',
-        plain: plainErrors.join(`\n\n`),
-        html: htmlErrors.join(`<br/><br/>`),
-        msgtype: 'm.text'
-    };
+    let alerts = [];
+    for (let i = 0; i < plainErrors.length; i++) {
+        alerts.push({
+            version: 'v2',
+            plain: plainErrors[i],
+            html: htmlErrors[i],
+            msgtype: 'm.text'
+        });
+    }
+    return alerts;
 }
 
 app.use(express.json())
@@ -127,18 +131,20 @@ app.post("/webhook/:id", async (req: Request, res: Response) => {
         logger.info(`Received data from the alertmanager for the id: ${req.params.id}`);
         var transforedData = transform(req.body as AlertData);
         // Post the data to the upstream hookshot service using the transformed data and the id given in the url
-        var response = await fetch(`${upstream}/${req.params.id}`,
-            {
-                method: "POST",
-                headers: new Headers({ 'content-type': 'application/json' }),
-                body: JSON.stringify(transforedData)
+        for (const data of transforedData) {
+            var response = await fetch(`${upstream}/${req.params.id}`,
+                {
+                    method: "POST",
+                    headers: new Headers({ 'content-type': 'application/json' }),
+                    body: JSON.stringify(data)
+                }
+            );
+            if (!response.ok) {
+                logger.error(`Failed to forward the data to the upstream service: ${response.text()}`);
+                res.status(500).send("Failed to forward the data to the upstream service");
+            } else {
+                res.status(200).send("Data forwarded successfully");
             }
-        );
-        if (!response.ok) {
-            logger.error(`Failed to forward the data to the upstream service: ${response.body}`);
-            res.status(500).send("Failed to forward the data to the upstream service");
-        } else {
-            res.status(200).send("Data forwarded successfully");
         }
     } catch (error) {
         logger.error(`Failed to forward the data to the upstream service: ${error}`);

commit b12ab0ff47fff6492013bca62ff00dbf548d4038

Fix batch re-sending failing after the 2nd item
An HTTP response was being sent after every item being processed,
causing the re-sending to break after the 2nd item due to a response
having already been sent on the stream.

Regression from c2e571cdaee4679c684fb388166da5a2f163cba0

index.ts
@@ -127,6 +127,7 @@ function transform(data: AlertData): { version: string, empty: boolean, msgtype:
 app.use(express.json())
 
 app.post("/webhook/:id", async (req: Request, res: Response) => {
+    var success = true;
     try {
         logger.info(`Received data from the alertmanager for the id: ${req.params.id}`);
         var transforedData = transform(req.body as AlertData);
@@ -141,13 +142,16 @@ app.post("/webhook/:id", async (req: Request, res: Response) => {
             );
             if (!response.ok) {
                 logger.error(`Failed to forward the data to the upstream service: ${response.text()}`);
-                res.status(500).send("Failed to forward the data to the upstream service");
-            } else {
-                res.status(200).send("Data forwarded successfully");
+                success = false;
             }
         }
     } catch (error) {
         logger.error(`Failed to forward the data to the upstream service: ${error}`);
+        success = false;
+    }
+    if (success) {
+        res.status(200).send("Data forwarded successfully");
+    } else {
         res.status(500).send("Failed to forward the data to the upstream service");
     }
 });